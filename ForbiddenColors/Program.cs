3517ba9f9bb22b05f050de3ed160010800f297b9
Alexis LOURS <37421461+alexislours@users.noreply.github.com>
2018-12-06 00:21:41 +0100

    Initial Commit

--- ForbiddenColors/Program.cs
+++ ForbiddenColors/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ForbiddenColors
 {
@@ -10,6 +8,43 @@ namespace ForbiddenColors
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the width of the bitmap :");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the height of the bitmap :");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the first color :");
+            string firstColor = Console.ReadLine();
+            Console.WriteLine("Enter the second color :");
+            string secondColor = Console.ReadLine();
+            Bitmap b = new Bitmap(width, height);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (x % 2 == 0)
+                    {
+                        Color c = b.GetPixel(x, y);
+                        b.SetPixel(x, y, Color.FromName(firstColor));
+                        y++;
+                        Color d = b.GetPixel(x, y);
+                        b.SetPixel(x, y, Color.FromName(secondColor));
+                    }
+                    else
+                    {
+                        Color d = b.GetPixel(x, y);
+                        b.SetPixel(x, y, Color.FromName(secondColor));
+                        y++;
+                        Color c = b.GetPixel(x, y);
+                        b.SetPixel(x, y, Color.FromName(firstColor));
+                    }
+                }
+                Console.WriteLine(x);
+            }
+            Console.Clear();
+            string imageName = ".\\" + firstColor + secondColor + width.ToString() + height.ToString() + ".png";
+            b.Save(imageName, ImageFormat.Png);
+            Console.WriteLine("File saved as : " + imageName);
+            Console.ReadKey();
         }
     }
 }
